aee4b1ff4ebc2e13b3ace8ff6b28425c211a6df9
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-03-20 11:53:45 +0200

    adding signup screen

---
+++ Chikonohono Clinic Management System/Create Account Form.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726")> _
+Partial Class Create_Account_Form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+    Friend WithEvents LogoPictureBox As System.Windows.Forms.PictureBox
+    Friend WithEvents UsernameLabel As System.Windows.Forms.Label
+    Friend WithEvents PasswordLabel As System.Windows.Forms.Label
+    Friend WithEvents UsernameTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents PasswordTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents btnCreate_Account As System.Windows.Forms.Button
+    Friend WithEvents Cancel As System.Windows.Forms.Button
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Create_Account_Form))
+        Me.LogoPictureBox = New System.Windows.Forms.PictureBox()
+        Me.UsernameLabel = New System.Windows.Forms.Label()
+        Me.PasswordLabel = New System.Windows.Forms.Label()
+        Me.UsernameTextBox = New System.Windows.Forms.TextBox()
+        Me.PasswordTextBox = New System.Windows.Forms.TextBox()
+        Me.btnCreate_Account = New System.Windows.Forms.Button()
+        Me.Cancel = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cb_usertype = New System.Windows.Forms.ComboBox()
+        Me.txtconfirmpassword = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CB_Show_Password = New System.Windows.Forms.CheckBox()
+        CType(Me.LogoPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LogoPictureBox
+        '
+        Me.LogoPictureBox.Image = CType(resources.GetObject("LogoPictureBox.Image"), System.Drawing.Image)
+        Me.LogoPictureBox.Location = New System.Drawing.Point(0, 0)
+        Me.LogoPictureBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.LogoPictureBox.Name = "LogoPictureBox"
+        Me.LogoPictureBox.Size = New System.Drawing.Size(219, 360)
+        Me.LogoPictureBox.TabIndex = 0
+        Me.LogoPictureBox.TabStop = False
+        '
+        'UsernameLabel
+        '
+        Me.UsernameLabel.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.UsernameLabel.Location = New System.Drawing.Point(288, 41)
+        Me.UsernameLabel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.UsernameLabel.Name = "UsernameLabel"
+        Me.UsernameLabel.Size = New System.Drawing.Size(330, 35)
+        Me.UsernameLabel.TabIndex = 0
+        Me.UsernameLabel.Text = "&User name"
+        Me.UsernameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'PasswordLabel
+        '
+        Me.PasswordLabel.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PasswordLabel.Location = New System.Drawing.Point(288, 112)
+        Me.PasswordLabel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.PasswordLabel.Name = "PasswordLabel"
+        Me.PasswordLabel.Size = New System.Drawing.Size(330, 35)
+        Me.PasswordLabel.TabIndex = 2
+        Me.PasswordLabel.Text = "&Password"
+        Me.PasswordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'UsernameTextBox
+        '
+        Me.UsernameTextBox.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.UsernameTextBox.Location = New System.Drawing.Point(291, 81)
+        Me.UsernameTextBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.UsernameTextBox.Name = "UsernameTextBox"
+        Me.UsernameTextBox.Size = New System.Drawing.Size(326, 26)
+        Me.UsernameTextBox.TabIndex = 1
+        '
+        'PasswordTextBox
+        '
+        Me.PasswordTextBox.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PasswordTextBox.Location = New System.Drawing.Point(291, 152)
+        Me.PasswordTextBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.PasswordTextBox.Name = "PasswordTextBox"
+        Me.PasswordTextBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(8226)
+        Me.PasswordTextBox.Size = New System.Drawing.Size(326, 26)
+        Me.PasswordTextBox.TabIndex = 3
+        '
+        'btnCreate_Account
+        '
+        Me.btnCreate_Account.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCreate_Account.Location = New System.Drawing.Point(476, 383)
+        Me.btnCreate_Account.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnCreate_Account.Name = "btnCreate_Account"
+        Me.btnCreate_Account.Size = New System.Drawing.Size(141, 35)
+        Me.btnCreate_Account.TabIndex = 4
+        Me.btnCreate_Account.Text = "&Create Account"
+        '
+        'Cancel
+        '
+        Me.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Cancel.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Cancel.Location = New System.Drawing.Point(291, 383)
+        Me.Cancel.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Cancel.Name = "Cancel"
+        Me.Cancel.Size = New System.Drawing.Size(141, 35)
+        Me.Cancel.TabIndex = 5
+        Me.Cancel.Text = "&Cancel"
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(287, 316)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(158, 35)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "&User type"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'cb_usertype
+        '
+        Me.cb_usertype.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cb_usertype.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cb_usertype.FormattingEnabled = True
+        Me.cb_usertype.Items.AddRange(New Object() {"Administrator", "Nurse"})
+        Me.cb_usertype.Location = New System.Drawing.Point(423, 319)
+        Me.cb_usertype.Name = "cb_usertype"
+        Me.cb_usertype.Size = New System.Drawing.Size(195, 28)
+        Me.cb_usertype.TabIndex = 6
+        '
+        'txtconfirmpassword
+        '
+        Me.txtconfirmpassword.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtconfirmpassword.Location = New System.Drawing.Point(291, 265)
+        Me.txtconfirmpassword.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtconfirmpassword.Name = "txtconfirmpassword"
+        Me.txtconfirmpassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(8226)
+        Me.txtconfirmpassword.Size = New System.Drawing.Size(326, 26)
+        Me.txtconfirmpassword.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(288, 225)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(330, 35)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Confirm &Password"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'CB_Show_Password
+        '
+        Me.CB_Show_Password.AutoSize = True
+        Me.CB_Show_Password.Location = New System.Drawing.Point(291, 186)
+        Me.CB_Show_Password.Name = "CB_Show_Password"
+        Me.CB_Show_Password.Size = New System.Drawing.Size(142, 24)
+        Me.CB_Show_Password.TabIndex = 13
+        Me.CB_Show_Password.Text = "Show &Password"
+        Me.CB_Show_Password.UseVisualStyleBackColor = True
+        '
+        'Create_Account_Form
+        '
+        Me.AcceptButton = Me.btnCreate_Account
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.Cancel
+        Me.ClientSize = New System.Drawing.Size(731, 432)
+        Me.Controls.Add(Me.CB_Show_Password)
+        Me.Controls.Add(Me.cb_usertype)
+        Me.Controls.Add(Me.Cancel)
+        Me.Controls.Add(Me.btnCreate_Account)
+        Me.Controls.Add(Me.txtconfirmpassword)
+        Me.Controls.Add(Me.PasswordTextBox)
+        Me.Controls.Add(Me.UsernameTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PasswordLabel)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.UsernameLabel)
+        Me.Controls.Add(Me.LogoPictureBox)
+        Me.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Create_Account_Form"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Create_Account_Form"
+        CType(Me.LogoPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents cb_usertype As System.Windows.Forms.ComboBox
+    Friend WithEvents txtconfirmpassword As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents CB_Show_Password As System.Windows.Forms.CheckBox
+
+End Class
---
+++ Chikonohono Clinic Management System/Create Account Form.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Data.OleDb
+Public Class Create_Account_Form
+
+    Dim provider As String
+    Dim datafile As String
+    Dim connString As String
+    Dim myconnection As OleDbConnection = New OleDbConnection
+    Dim str As String
+
+    Private Sub btnCreate_Account_Click(sender As Object, e As EventArgs) Handles btnCreate_Account.Click
+        provider = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source ="
+        datafile = "|DataDirectory|\ccms.mdb"
+        connString = provider & datafile
+        myconnection.ConnectionString = connString
+        myconnection.Open()
+
+        If PasswordTextBox.Text <> txtconfirmpassword.Text Then
+            MsgBox("Please confirm your password", MsgBoxStyle.Exclamation)
+            txtconfirmpassword.Clear()
+            PasswordTextBox.Clear()
+        Else
+            str = "insert into [user info] ([Username], [Password], [User type]) values (?, ?, ?)"
+            Dim cmd As OleDbCommand = New OleDbCommand(str, myconnection)
+            cmd.Parameters.Add(New OleDbParameter("Username", CType(UsernameTextBox.Text, String)))
+            cmd.Parameters.Add(New OleDbParameter("Password", CType(PasswordTextBox.Text, String)))
+            cmd.Parameters.Add(New OleDbParameter("User type", CType(cb_usertype.Text, String)))
+
+            Try
+                cmd.ExecuteNonQuery()
+                cmd.Dispose()
+                myconnection.Close()
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+            If cb_usertype.Text = "" Then
+                MsgBox("Please select user type.", MsgBoxStyle.Exclamation)
+            Else
+                MsgBox("User Successfully added", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "User Registration")
+
+            End If
+            myconnection.Close()
+
+        End If
+    End Sub
+
+
+    Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
+        Main_Window.Show()
+        Me.Close()
+    End Sub
+
+
+    Private Sub CB_Show_Password_CheckedChanged(sender As Object, e As EventArgs)
+        If CB_Show_Password.Checked = True Then
+            PasswordTextBox.PasswordChar = ""
+        Else
+            PasswordTextBox.PasswordChar = "•"
+        End If
+    End Sub
+End Class
