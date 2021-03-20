15f1090b1c8df822fc69de1f2d161634e86c7b5b
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-03-20 11:44:08 +0200

    setting up login screen and DB

---
+++ Chikonohono Clinic Management System/Login Form.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726")> _
+Partial Class Login_Form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+    Friend WithEvents UsernameLabel As System.Windows.Forms.Label
+    Friend WithEvents PasswordLabel As System.Windows.Forms.Label
+    Friend WithEvents UsernameTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents PasswordTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents btnLogin As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Login_Form))
+        Me.UsernameLabel = New System.Windows.Forms.Label()
+        Me.PasswordLabel = New System.Windows.Forms.Label()
+        Me.UsernameTextBox = New System.Windows.Forms.TextBox()
+        Me.PasswordTextBox = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.Attempts = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.cb_usertype = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CB_Show_Password = New System.Windows.Forms.CheckBox()
+        Me.StatusStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'UsernameLabel
+        '
+        Me.UsernameLabel.Location = New System.Drawing.Point(271, 33)
+        Me.UsernameLabel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.UsernameLabel.Name = "UsernameLabel"
+        Me.UsernameLabel.Size = New System.Drawing.Size(293, 30)
+        Me.UsernameLabel.TabIndex = 0
+        Me.UsernameLabel.Text = "&User name"
+        Me.UsernameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'PasswordLabel
+        '
+        Me.PasswordLabel.Location = New System.Drawing.Point(273, 111)
+        Me.PasswordLabel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.PasswordLabel.Name = "PasswordLabel"
+        Me.PasswordLabel.Size = New System.Drawing.Size(293, 30)
+        Me.PasswordLabel.TabIndex = 2
+        Me.PasswordLabel.Text = "&Password"
+        Me.PasswordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'UsernameTextBox
+        '
+        Me.UsernameTextBox.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.UsernameTextBox.Location = New System.Drawing.Point(274, 67)
+        Me.UsernameTextBox.Margin = New System.Windows.Forms.Padding(4)
+        Me.UsernameTextBox.Name = "UsernameTextBox"
+        Me.UsernameTextBox.Size = New System.Drawing.Size(292, 26)
+        Me.UsernameTextBox.TabIndex = 1
+        '
+        'PasswordTextBox
+        '
+        Me.PasswordTextBox.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PasswordTextBox.Location = New System.Drawing.Point(276, 137)
+        Me.PasswordTextBox.Margin = New System.Windows.Forms.Padding(4)
+        Me.PasswordTextBox.Name = "PasswordTextBox"
+        Me.PasswordTextBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(8226)
+        Me.PasswordTextBox.Size = New System.Drawing.Size(292, 26)
+        Me.PasswordTextBox.TabIndex = 3
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(443, 254)
+        Me.btnLogin.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(125, 30)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "&Login"
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(292, 254)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(125, 30)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "&Exit"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Attempts})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 290)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(668, 22)
+        Me.StatusStrip1.TabIndex = 7
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'Attempts
+        '
+        Me.Attempts.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Attempts.LinkColor = System.Drawing.Color.Black
+        Me.Attempts.Name = "Attempts"
+        Me.Attempts.Size = New System.Drawing.Size(96, 17)
+        Me.Attempts.Text = "Attempts Left :"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(47, 33)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(166, 229)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'cb_usertype
+        '
+        Me.cb_usertype.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cb_usertype.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cb_usertype.FormattingEnabled = True
+        Me.cb_usertype.Items.AddRange(New Object() {"Administrator", "Nurse"})
+        Me.cb_usertype.Location = New System.Drawing.Point(373, 197)
+        Me.cb_usertype.Name = "cb_usertype"
+        Me.cb_usertype.Size = New System.Drawing.Size(195, 28)
+        Me.cb_usertype.TabIndex = 10
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(271, 194)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(158, 35)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "&User type"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'CB_Show_Password
+        '
+        Me.CB_Show_Password.AutoSize = True
+        Me.CB_Show_Password.Location = New System.Drawing.Point(276, 170)
+        Me.CB_Show_Password.Name = "CB_Show_Password"
+        Me.CB_Show_Password.Size = New System.Drawing.Size(127, 21)
+        Me.CB_Show_Password.TabIndex = 11
+        Me.CB_Show_Password.Text = "Show &Password"
+        Me.CB_Show_Password.UseVisualStyleBackColor = True
+        '
+        'Login_Form
+        '
+        Me.AcceptButton = Me.btnLogin
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(668, 312)
+        Me.Controls.Add(Me.CB_Show_Password)
+        Me.Controls.Add(Me.cb_usertype)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.PasswordTextBox)
+        Me.Controls.Add(Me.UsernameTextBox)
+        Me.Controls.Add(Me.PasswordLabel)
+        Me.Controls.Add(Me.UsernameLabel)
+        Me.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Login_Form"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login Form"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents Attempts As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents cb_usertype As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents CB_Show_Password As System.Windows.Forms.CheckBox
+
+End Class
---
+++ Chikonohono Clinic Management System/Login Form.vb
@@ -0,0 +1,75 @@
+﻿Imports System.Data.OleDb
+
+Public Class Login_Form
+    'declaration of variables
+    Dim con As OleDbConnection, cmd As OleDbCommand
+    Dim provider As String
+    Dim datafile As String
+    Dim str As String
+
+#Region "Variable"
+    Public Shared counter As Integer = 3
+    Dim x As String = "Attempts Left : "
+
+#End Region
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+
+        'initialising the connection string
+        con = New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source= |DataDirectory|\ccms.mdb")
+        con.Open()
+        str = "Select * from [user info] WHERE [Username] like '" & Trim(UsernameTextBox.Text) & _
+       "' and [Password] like '" & Trim(PasswordTextBox.Text) & "' and [User type] like '" & Trim(cb_usertype.Text) & "';"
+
+        cmd = New OleDbCommand(str, con)
+        Dim dr As OleDbDataReader
+        dr = cmd.ExecuteReader()
+
+        If dr.HasRows = False Then
+            MsgBox("Invalid Login Details. Please Provide your Login Details", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly)
+            fields_clear()
+            counter = counter - 1
+            Attempts.Text = x & counter
+
+            If counter = 0 Then
+                MsgBox("Unauthorised user. System Shutting Down", MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "System Shutdown")
+                Application.Exit()
+            End If
+        End If
+
+        While (dr.Read())
+            If (UsernameTextBox.Text = dr(0).ToString()) Or (PasswordTextBox.Text = dr(1).ToString()) And (cb_usertype.Text = dr(2).ToString()) Then
+                MsgBox("Successfully Logged in, Welcome.", MessageBoxIcon.Information, "Login Successful.")
+            End If
+            Main_Window.Show()
+            Me.Close()
+        End While
+        dr.Close()
+        cmd = Nothing
+        con.Close()
+    End Sub
+
+    Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        End
+    End Sub
+
+
+    Private Sub LoginForm1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Attempts.Text = x & counter
+    End Sub
+
+    Private Sub fields_clear()
+        UsernameTextBox.Clear()
+        PasswordTextBox.Clear()
+        cb_usertype.SelectedIndex = -1
+    End Sub
+
+    Private Sub CB_Show_Password_CheckedChanged(sender As Object, e As EventArgs) Handles CB_Show_Password.CheckedChanged
+        If CB_Show_Password.Checked = True Then
+            PasswordTextBox.PasswordChar = ""
+        Else
+            PasswordTextBox.PasswordChar = "•"
+        End If
+    End Sub
+
+End Class
