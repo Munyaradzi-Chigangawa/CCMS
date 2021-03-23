42e0bc17473ea2aee0b979b1546cdc10edced1fb
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-03-24 00:02:58 +0200

    existing patient records

---
+++ Chikonohono Clinic Management System/Existing Patient Records.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Existing_Patients_Records
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Existing_Patients_Records))
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Patient_Records = New System.Windows.Forms.Label()
+        Me.Back = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.GridColor = System.Drawing.SystemColors.AppWorkspace
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 82)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(1330, 553)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Patient_Records
+        '
+        Me.Patient_Records.AutoSize = True
+        Me.Patient_Records.Font = New System.Drawing.Font("Century Gothic", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Patient_Records.Location = New System.Drawing.Point(532, 21)
+        Me.Patient_Records.Name = "Patient_Records"
+        Me.Patient_Records.Size = New System.Drawing.Size(287, 42)
+        Me.Patient_Records.TabIndex = 10
+        Me.Patient_Records.Text = "Patient Records"
+        '
+        'Back
+        '
+        Me.Back.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Back.Location = New System.Drawing.Point(1211, 654)
+        Me.Back.Name = "Back"
+        Me.Back.Size = New System.Drawing.Size(131, 38)
+        Me.Back.TabIndex = 11
+        Me.Back.Text = "Back"
+        Me.Back.UseVisualStyleBackColor = True
+        '
+        'Existing_Patients_Records
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(1354, 716)
+        Me.Controls.Add(Me.Back)
+        Me.Controls.Add(Me.Patient_Records)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Existing_Patients_Records"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Existing Patients Records"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Patient_Records As System.Windows.Forms.Label
+    Friend WithEvents Back As System.Windows.Forms.Button
+End Class
---
+++ Chikonohono Clinic Management System/Existing Patient Records.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Data.OleDb
+
+Public Class Existing_Patients_Records
+
+    Dim Con As New OleDbConnection
+    Private Sub Patients_Records_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Con.ConnectionString = ("Provider=Microsoft.Jet.OLEDB.4.0; Data Source=|DataDirectory|/ccms.mdb")
+        Con.Open()
+
+        Show_Records()
+
+    End Sub
+    Private Sub Show_Records()
+        Dim ds As New DataSet
+        Dim dt As New DataTable
+        ds.Tables.Add(dt)
+        Dim da As New OleDbDataAdapter
+
+        da = New OleDbDataAdapter("select * from [patient records]", Con)
+        da.Fill(dt)
+
+        DataGridView1.DataSource = dt.DefaultView
+        Con.Close()
+
+
+    End Sub
+
+
+    Private Sub Back_Click(sender As Object, e As EventArgs) Handles Back.Click
+        Patient_Records_Menu.Show()
+        Me.Close()
+    End Sub
+End Class
