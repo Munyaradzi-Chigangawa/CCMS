69afdd3818328d3e835b1aab75957c386c6977d9
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-04-03 14:41:19 +0200

    existing medication records

---
+++ Chikonohono Clinic Management System/Existing Medication Records.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Existing_Medication_Records
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblPatient_Registration = New System.Windows.Forms.Label()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblPatient_Registration
+        '
+        Me.lblPatient_Registration.AutoSize = True
+        Me.lblPatient_Registration.Font = New System.Drawing.Font("Century Gothic", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPatient_Registration.Location = New System.Drawing.Point(411, 35)
+        Me.lblPatient_Registration.Name = "lblPatient_Registration"
+        Me.lblPatient_Registration.Size = New System.Drawing.Size(532, 42)
+        Me.lblPatient_Registration.TabIndex = 8
+        Me.lblPatient_Registration.Text = "Available Medication Records"
+        '
+        'btnBack
+        '
+        Me.btnBack.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnBack.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBack.Location = New System.Drawing.Point(1236, 655)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(92, 39)
+        Me.btnBack.TabIndex = 9
+        Me.btnBack.Text = "Back"
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(152, 181)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(1051, 420)
+        Me.DataGridView1.TabIndex = 10
+        '
+        'Existing_Medication_Records
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1354, 717)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnBack)
+        Me.Controls.Add(Me.lblPatient_Registration)
+        Me.Name = "Existing_Medication_Records"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Existing_Medication_Records"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblPatient_Registration As System.Windows.Forms.Label
+    Friend WithEvents btnBack As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+End Class
---
+++ Chikonohono Clinic Management System/Existing Medication Records.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data.OleDb
+Public Class Existing_Medication_Records
+    Dim con As New OleDbConnection
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        Dispensary.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub Existing_Medication_Records_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con.ConnectionString = ("Provider=Microsoft.Jet.OLEDB.4.0; Data Source=|DataDirectory|/ccms.mdb")
+        Con.Open()
+
+        Show_Records()
+    End Sub
+    Public Sub Show_Records()
+        Dim ds As New DataSet
+        Dim dt As New DataTable
+        ds.Tables.Add(dt)
+        Dim da As New OleDbDataAdapter
+
+        da = New OleDbDataAdapter("select * from [dispensary]", con)
+        da.Fill(dt)
+
+        DataGridView1.DataSource = dt.DefaultView
+        con.Close()
+    End Sub
+End Class
