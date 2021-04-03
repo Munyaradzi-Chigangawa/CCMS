7f0da2c84e66edc274a9d21262a32664bbe285a3
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-04-03 14:39:46 +0200

    medication records

---
+++ Chikonohono Clinic Management System/Add Medication Form.Designer.vb
@@ -0,0 +1,265 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Add_Medication_Form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCode = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtQuantity = New System.Windows.Forms.TextBox()
+        Me.txtDescription = New System.Windows.Forms.TextBox()
+        Me.txtHandler = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.DTPManufacture_date = New System.Windows.Forms.DateTimePicker()
+        Me.DTPExpiry_Date = New System.Windows.Forms.DateTimePicker()
+        Me.SuspendLayout()
+        '
+        'btnSave
+        '
+        Me.btnSave.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btnSave.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Location = New System.Drawing.Point(482, 622)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(123, 36)
+        Me.btnSave.TabIndex = 3
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btnClear.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(630, 622)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(105, 36)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnBack
+        '
+        Me.btnBack.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btnBack.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBack.Location = New System.Drawing.Point(767, 622)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(105, 36)
+        Me.btnBack.TabIndex = 4
+        Me.btnBack.Text = "Back"
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(547, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(260, 42)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Add Medicine"
+        '
+        'txtCode
+        '
+        Me.txtCode.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCode.Location = New System.Drawing.Point(638, 178)
+        Me.txtCode.Name = "txtCode"
+        Me.txtCode.Size = New System.Drawing.Size(215, 26)
+        Me.txtCode.TabIndex = 6
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(638, 239)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(215, 26)
+        Me.txtName.TabIndex = 6
+        '
+        'txtQuantity
+        '
+        Me.txtQuantity.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtQuantity.Location = New System.Drawing.Point(638, 299)
+        Me.txtQuantity.Name = "txtQuantity"
+        Me.txtQuantity.Size = New System.Drawing.Size(215, 26)
+        Me.txtQuantity.TabIndex = 6
+        '
+        'txtDescription
+        '
+        Me.txtDescription.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDescription.Location = New System.Drawing.Point(638, 358)
+        Me.txtDescription.Name = "txtDescription"
+        Me.txtDescription.Size = New System.Drawing.Size(215, 26)
+        Me.txtDescription.TabIndex = 6
+        '
+        'txtHandler
+        '
+        Me.txtHandler.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtHandler.Location = New System.Drawing.Point(638, 417)
+        Me.txtHandler.Name = "txtHandler"
+        Me.txtHandler.Size = New System.Drawing.Size(215, 26)
+        Me.txtHandler.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(502, 185)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 17)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Code"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(502, 246)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 17)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(502, 306)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(63, 17)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Quantity"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(502, 365)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(80, 17)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Description"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(502, 424)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(58, 17)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Handler"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(502, 483)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(126, 17)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Manufacture Date"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(502, 549)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(79, 17)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Expiry Date"
+        '
+        'DTPManufacture_date
+        '
+        Me.DTPManufacture_date.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DTPManufacture_date.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DTPManufacture_date.Location = New System.Drawing.Point(638, 477)
+        Me.DTPManufacture_date.Name = "DTPManufacture_date"
+        Me.DTPManufacture_date.Size = New System.Drawing.Size(215, 23)
+        Me.DTPManufacture_date.TabIndex = 8
+        '
+        'DTPExpiry_Date
+        '
+        Me.DTPExpiry_Date.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DTPExpiry_Date.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DTPExpiry_Date.Location = New System.Drawing.Point(638, 543)
+        Me.DTPExpiry_Date.Name = "DTPExpiry_Date"
+        Me.DTPExpiry_Date.Size = New System.Drawing.Size(215, 23)
+        Me.DTPExpiry_Date.TabIndex = 8
+        '
+        'Add_Medication_Form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(1354, 716)
+        Me.Controls.Add(Me.DTPExpiry_Date)
+        Me.Controls.Add(Me.DTPManufacture_date)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtHandler)
+        Me.Controls.Add(Me.txtDescription)
+        Me.Controls.Add(Me.txtQuantity)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.txtCode)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnBack)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSave)
+        Me.Name = "Add_Medication_Form"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Add_Medication_Form"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnBack As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtCode As System.Windows.Forms.TextBox
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents txtQuantity As System.Windows.Forms.TextBox
+    Friend WithEvents txtDescription As System.Windows.Forms.TextBox
+    Friend WithEvents txtHandler As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents DTPManufacture_date As System.Windows.Forms.DateTimePicker
+    Friend WithEvents DTPExpiry_Date As System.Windows.Forms.DateTimePicker
+End Class
---
+++ Chikonohono Clinic Management System/Add Medication Form.vb
@@ -0,0 +1,123 @@
+﻿Imports System.Data.OleDb
+Public Class Add_Medication_Form
+    Dim provider As String
+    Dim datafile As String
+    Dim connString As String
+    Dim myconnection As OleDbConnection = New OleDbConnection
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        Dispensary.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        Call Clear()
+
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        provider = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source ="
+        datafile = "|DataDirectory|\ccms.mdb"
+        connString = provider & datafile
+        myconnection.ConnectionString = connString
+        myconnection.Open()
+
+        Dim str As String
+
+        str = "insert into [dispensary] ([Code], [Name], [Quantity], [Description], [Handler], [Manufacture Date],[Expiry Date]) values (?, ?, ?, ?, ?, ?, ?)"
+        Dim cmd As OleDbCommand = New OleDbCommand(str, myconnection)
+        cmd.Parameters.Add(New OleDbParameter("Code", CType(txtCode.Text, String)))
+        cmd.Parameters.Add(New OleDbParameter("Name", CType(txtName.Text, String)))
+        cmd.Parameters.Add(New OleDbParameter("Quantity", CType(txtQuantity.Text, String)))
+        cmd.Parameters.Add(New OleDbParameter("Description", CType(txtDescription.Text, String)))
+        cmd.Parameters.Add(New OleDbParameter("Handler", CType(txtHandler.Text, String)))
+        cmd.Parameters.Add(New OleDbParameter("Manufacture Date", CType(DTPManufacture_date.Text, String)))
+        cmd.Parameters.Add(New OleDbParameter("Expiry Date", CType(DTPExpiry_Date.Text, String)))
+
+        Try
+            cmd.ExecuteNonQuery()
+            cmd.Dispose()
+            myconnection.Close()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        myconnection.Close()
+        MsgBox("Mecication Record Successfully added.", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Successful")
+        Call Clear()
+    End Sub
+
+    Public Sub Clear()
+        txtCode.Clear()
+        txtName.Clear()
+        txtQuantity.Clear()
+        txtHandler.Clear()
+        txtDescription.Clear()
+        DTPManufacture_date.Value = Date.Now
+        DTPExpiry_Date.Value = Date.Now
+    End Sub
+
+    Private Sub txtCode_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtCode.KeyPress
+
+    End Sub
+
+    Private Sub txtQuantity_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtQuantity.KeyPress
+        If (Char.IsControl(e.KeyChar) = False) Then
+            If (Char.IsDigit(e.KeyChar)) Then
+                'do nothing
+
+
+            Else
+                e.Handled = True
+                MsgBox("Sorry Only Digits Allowed!!", MsgBoxStyle.Information, "Verify")
+                txtQuantity.Focus()
+            End If
+        End If
+    End Sub
+
+    Private Sub txtName_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtName.KeyPress
+        If (Char.IsControl(e.KeyChar) = False) Then
+            If (Char.IsLetter(e.KeyChar)) Or (Char.IsWhiteSpace(e.KeyChar)) Then
+                'do nothing
+
+            Else
+                e.Handled = True
+                MsgBox("Sorry Only Character & Spaces Allowed!!", MsgBoxStyle.Information, "Verify")
+                txtName.Focus()
+
+            End If
+        End If
+
+    End Sub
+
+    Private Sub txtDescription_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtDescription.KeyPress
+        If (Char.IsControl(e.KeyChar) = False) Then
+            If (Char.IsLetter(e.KeyChar)) Or (Char.IsWhiteSpace(e.KeyChar)) Then
+                'do nothing
+
+            Else
+                e.Handled = True
+                MsgBox("Sorry Only Character & Spaces Allowed!!", MsgBoxStyle.Information, "Verify")
+                txtDescription.Focus()
+
+            End If
+        End If
+
+    End Sub
+
+    Private Sub txtHandler_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtHandler.KeyPress
+        If (Char.IsControl(e.KeyChar) = False) Then
+            If (Char.IsLetter(e.KeyChar)) Or (Char.IsWhiteSpace(e.KeyChar)) Then
+                'do nothing
+
+            Else
+                e.Handled = True
+                MsgBox("Sorry Only Character & Spaces Allowed!!", MsgBoxStyle.Information, "Verify")
+                txtHandler.Focus()
+
+            End If
+        End If
+
+    End Sub
+
+End Class
