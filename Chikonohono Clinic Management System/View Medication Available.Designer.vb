d052d578be3532d5d67ce92d0a023bff5ce614df
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-04-03 15:18:52 +0200

    adding files

---
+++ Chikonohono Clinic Management System/View Medication Available.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Existing_Medication_Available
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Delete = New System.Windows.Forms.Button()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Delete
+        '
+        Me.Delete.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Delete.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Delete.Location = New System.Drawing.Point(1010, 641)
+        Me.Delete.Name = "Delete"
+        Me.Delete.Size = New System.Drawing.Size(123, 36)
+        Me.Delete.TabIndex = 5
+        Me.Delete.Text = "Delete"
+        Me.Delete.UseVisualStyleBackColor = True
+        '
+        'btnBack
+        '
+        Me.btnBack.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnBack.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBack.Location = New System.Drawing.Point(1168, 641)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(123, 36)
+        Me.btnBack.TabIndex = 5
+        Me.btnBack.Text = "Back"
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'Existing_Medication_Available
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1354, 716)
+        Me.Controls.Add(Me.btnBack)
+        Me.Controls.Add(Me.Delete)
+        Me.Name = "Existing_Medication_Available"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Existing_Medication_Available"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Delete As System.Windows.Forms.Button
+    Friend WithEvents btnBack As System.Windows.Forms.Button
+End Class
---
+++ Chikonohono Clinic Management System/View Medication Available.vb
@@ -0,0 +1,7 @@
+﻿Public Class Existing_Medication_Available
+
+    Private Sub Delete_Click(sender As Object, e As EventArgs) Handles Delete.Click, btnBack.Click
+        Medication_Available.Show()
+        Me.Close()
+    End Sub
+End Class
